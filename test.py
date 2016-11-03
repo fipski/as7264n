d476c91831e3da7c861763a561e747c415d41626
Philipp <philipp.vt@gmail.com>
2016-11-03 20:16:03 +0100

    testing

--- test.py
+++ test.py
@@ -2,14 +2,25 @@
 
 import colsens
 import colormath
+from timeit import default_timer as timer
 
 # test setup and config
 colsens.setMux(0)
 colsens.setup_col_sens()
-temp = colsens.getTemp()
-print("Temperature: " + str(temp))
-colsens.setIntTime(0)
-colsens.setGain(0b10)
+# temp = colsens.getTemp()
+# print("Temperature: " + str(temp))
+colsens.setIntTime(255)
+colsens.setGain(0b11)
+colsens.setWtime(0)
+colsens.setBank(0)
+colsens.autoZero()
+# setup sens 2
+colsens.setMux(1)
+colsens.setup_col_sens()
+# temp = colsens.getTemp()
+# print("Temperature: " + str(temp))
+colsens.setIntTime(255)
+colsens.setGain(0b11)
 colsens.setWtime(0)
 colsens.setBank(0)
 colsens.autoZero()
@@ -36,17 +47,31 @@ def convertXYZtoxyY(X, Y, Z):
     y = Y / (X + Y + Z)
     return x, y, Z
 
-while 1:
-    X, Y, Z, N = colsens.doMeasure(1)
-    X, Y, Z = calibrate(X, Y, Z)
-    X, Y, Z = normalize(X, Y, Z)
-    x, y, Y = convertXYZtoxyY(X, Y, Z)
+for i in range (0,2):
+    print("X_" +str(i)+ ";", end = '')
+    print("Y_" +str(i)+ ";", end = '')
+    print("Z_" +str(i)+ ";", end = '')
+    print("Temperature_" +str(i)+ ";", end = '')
+    print("Time_" +str(i)+ ";", end = '')
+print("")
 
-    print("x: " + str(x) )
-    print("y: " + str(y) )
-    print("Y: " + str(Y) )
-    temp = colsens.getTemp()
-    print("Temperature: " + str(temp))
+while 1:
+    for i in range (0,2):
+        colsens.setMux(i)
+        start = timer()
+        X, Y, Z, N = colsens.doMeasure(1)
+        # X, Y, Z = calibrate(X, Y, Z)
+        # X, Y, Z = normalize(X, Y, Z)
+        # x, y, Y = convertXYZtoxyY(X, Y, Z)
+        end = timer()
+        temp = colsens.getTemp()
+        time = (end - start)
+        end, start = 0,0
+        print(str(X)+';' , end = '')
+        print(str(Y)+';' , end = '')
+        print(str(Z)+';' , end = '')
+        print(str(temp)+';', end = '')
+        print(str(time)+';', end = '')
     print("")
 
 # from colormath.color_objects import XYZColor, HSLColor, AdobeRGBColor
